aed5b40412fe806e4d8c645ca360df1e3c7d340e
ToniRajamaki <zingoo16@gmail.com>
2020-09-14 10:42:20 +0300

    NN-classifier function completed

--- cifar10_illustrate.py
+++ cifar10_illustrate.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
 from PIL import Image
 
 
@@ -12,7 +13,7 @@ def class_acc(pred,gt):
     truePositive = 0
     pictureQuantity = len(gt)
     for i in range(pictureQuantity):
-        print(pred[i], gt[i])
+       # print(pred[i], gt[i])
         if pred[i] == gt[i]:
             truePositive = truePositive + 1
 
@@ -23,9 +24,9 @@ def class_acc(pred,gt):
 
 # chooses random class from temp data, then calls class_acc with prediction array
 # of randomclass
-def cifar10_classifier_random(x):
+def cifar10_classifier_random(dataSet):
 
-    randomClassFromTestData = random.choice(x)
+    randomClassFromTestData = random.choice(dataSet)
     return randomClassFromTestData
 
 
@@ -39,7 +40,6 @@ def convert_images_to_1d_arrays(images_array):
         img = images_array[i]
         oneD_image_array = img.reshape(rows * cols * colors) # from 3 matrixes to 1-D array
         converted_images.append(oneD_image_array)
-        print(oneD_image_array)
 
     return converted_images
 
@@ -53,25 +53,35 @@ def unpickle(file):
 picturesIterated = 1
 
 datadict = unpickle('cifar-10-batches-py/data_batch_1')
-#datadict = unpickle('cifar-10-batches-py/test_batch')
-
-
-X = datadict["data"]
-Y = datadict["labels"]
+X_training = datadict["data"]
+Y_training = datadict["labels"]
 labeldict = unpickle('cifar-10-batches-py/batches.meta')
 label_names = labeldict["label_names"]
-X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
-Y = np.array(Y)
+X_training = X_training.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+Y_training = np.array(Y_training)
+training_image_classes = Y_training
 
-groundTruths = Y[0:picturesIterated]
+test_datadict = unpickle('cifar-10-batches-py/test_batch')
+X_test = test_datadict["data"]
+Y_test = test_datadict["labels"]
+labeldict = unpickle('cifar-10-batches-py/batches.meta')
+label_names = labeldict["label_names"]
+X_test = X_test.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+Y_test = np.array(Y_test)
+
+
+
+
+
+#turha varmaa
+#groundTruths = Y_training[0:picturesIterated]
 
 def test_class_acc_with_random_array():
     #creating prediction array from randomclass
-    randomClass = cifar10_classifier_random(Y)
+    randomClass = cifar10_classifier_random(Y_training)
     temp = [randomClass]
-    randomPredictionArray = len(Y) * temp
-
-    class_acc(randomPredictionArray,Y)
+    randomPredictionArray = len(Y_training) * temp
+    class_acc(randomPredictionArray,Y_training)
 
 
 def for_each_picture():
@@ -81,14 +91,49 @@ def for_each_picture():
         if True:
             plt.figure(1);
             plt.clf()
-            plt.imshow(X[i])
-            plt.title(f"Image {i} label={label_names[Y[i]]} (num {Y[i]})")
-            plt.pause(0.1)
+            #plt.imshow(X_training[i])
+            plt.title(f"Image {i} label={label_names[Y_training[i]]} (num {Y_training[i]})")
+            #plt.pause(0.1)
+
 
 
 for_each_picture()
 
-training_images = X[0:20]
-converted_test_images = convert_images_to_1d_arrays(training_images)
+image_quantity = 1500
+split_Y_test = Y_test[0:image_quantity]
+
+training_images = X_training[0:image_quantity]
+converted_training_images = convert_images_to_1d_arrays(training_images)
+
+test_images = X_test[0:image_quantity]
+converted_test_images = convert_images_to_1d_arrays(test_images)
 
 
+def cifar_10():
+    prediction_array = []
+    count = 0
+
+    for i in range(image_quantity):
+        count = count + 1
+        print(count,' / ',image_quantity)
+        distance, predicted_class = closest_distance(test_images[i],training_images,training_image_classes,image_quantity)
+
+        prediction_array.append(predicted_class)
+
+    class_acc(prediction_array,split_Y_test)
+    return
+
+def closest_distance(test_image, training_images,training_image_classes, image_quantity):
+    closest_distance_so_far = math.inf
+    prediction_class = 10
+
+    for i in range(image_quantity):
+
+        distance = np.sum(np.square(test_image - training_images[i]))
+        if distance < closest_distance_so_far:
+            closest_distance_so_far = distance
+            prediction_class = training_image_classes[i]
+    return closest_distance_so_far, prediction_class
+
+
+cifar_10()
