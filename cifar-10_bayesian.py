69e6df36518685f33fe5c96d2ff0f77982ea0648
ToniRajamaki <zingoo16@gmail.com>
2020-09-19 12:24:16 +0300

    Extracted for loop for a method and more refactoring

--- cifar-10_bayesian.py
+++ cifar-10_bayesian.py
@@ -10,7 +10,7 @@ def unpickle(file):
         dict = pickle.load(f, encoding="latin1")
     return dict
 
-
+# reading some metainfo
 labeldict = unpickle('cifar-10-batches-py/batches.meta')
 label_names = labeldict["label_names"]
 
@@ -21,33 +21,38 @@ def get_training_data(string):
     classes = datadict["labels"] #Classes
     return images,classes
 
+# images ( quanity , 32 , 32 ,3 )
+def cifar_10_color(image_dataset):
 
+    image_quanity = (image_dataset.shape[0]) # How many images we are processing
+    image_rgb_mean = np.zeros((image_quanity, 3))
+
+    for i in range(image_quanity):
+        # Convert images to mean values of each color channel
+        img = image_dataset[i]
+        img_8x8 = resize(img, (8, 8))  # this is probably useless
+        img_1x1 = resize(img, (1, 1))
+        r_vals = img_1x1[:, :, 0].reshape(1 * 1)
+        g_vals = img_1x1[:, :, 1].reshape(1 * 1)
+        b_vals = img_1x1[:, :, 2].reshape(1 * 1)
+        mu_r = r_vals.mean()  # mean value of this color channel for 1 image (decimal 0 - 1
+        mu_g = g_vals.mean()
+        mu_b = b_vals.mean()
+
+        image_rgb_mean[i, :] = (mu_r, mu_g, mu_b)  # shape = ( imagequanity, 3 )
+
+    return image_rgb_mean
+
+
+#Getting the training data from batch 1
 images_1, classes_1 = get_training_data('cifar-10-batches-py/data_batch_1')
-
-
-
 images_1 = images_1.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("uint8")
-
 classes_1 = np.array(classes_1)
 
-X_mean = np.zeros((10000, 3))
-for i in range(images_1.shape[0]):
-    # Convert images to mean values of each color channel
-    img = images_1[i]
-    img_8x8 = resize(img, (8, 8))
-    img_1x1 = resize(img, (1, 1))
-    r_vals = img_1x1[:, :, 0].reshape(1 * 1)
-    g_vals = img_1x1[:, :, 1].reshape(1 * 1)
-    b_vals = img_1x1[:, :, 2].reshape(1 * 1)
-    mu_r = r_vals.mean()
-    mu_g = g_vals.mean()
-    mu_b = b_vals.mean()
-    X_mean[i, :] = (mu_r, mu_g, mu_b)
+# How many pictures we take from the batch ( 1 - 10,000 )
+DATA_SET_QUANITY = 500;
+images_1 = images_1[0:DATA_SET_QUANITY]
+classes_1 = classes_1[0:DATA_SET_QUANITY]
 
-    # Show some images randomly
-    if random() > 0.999:
-        plt.figure(1);
-        plt.clf()
-        plt.imshow(img_8x8)
-        plt.title(f"Image {i} label={label_names[classes_1[i]]} (num {classes_1[i]})")
-        plt.pause(1)
+rgb_means = cifar_10_color(images_1)
+print(rgb_means.shape)
